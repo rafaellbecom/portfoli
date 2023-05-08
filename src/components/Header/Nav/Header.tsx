4c992ff668c0ce76de8b9d9e927b3544a7eef79f
Rafael Lima <rafaellbecom@gmail.com>
2023-05-08 19:17:14 -0300

    :lipstick: Formatando estilo do menu, /src/components/Nav/Nav.tsx

--- src/components/Header/Nav/Header.tsx
+++ src/components/Header/Nav/Header.tsx
@@ -2,9 +2,9 @@ import { Nav } from 'components/Header/Nav/Nav';
 
 export function Header(){
     return(
-        <header className="w-full h-[100px] bg-black flex items-center items-center">            
-            <div className="container m-auto flex justify-between">
-                <h1 className="text-white text-2xl font-bold">RAFA<span className="font-medium italic">DEV</span></h1>
+        <header className="w-full h-[80px] flex items-center items-center">            
+            <div className="container m-auto flex justify-between items-center">
+                <h1 className="text-white text-3xl font-bold">RAFA<span className="font-medium italic">DEV</span></h1>
                 <Nav></Nav>
             </div>
         </header>
--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -1,15 +1,15 @@
 export function Nav(){
     return(
-        <nav className="flex items-center">
-            <ul>
-                <li><a href="#" className="linkMenu">Home</a></li>
-                <li><a href="#" className="linkMenu">Sobre mim</a></li>
-                <li><a href="#" className="linkMenu">Tecnologias</a></li>
-                <li><a href="#" className="linkMenu">Projetos</a></li>
-                <li><a href="#" className="linkMenu">Skills</a></li>
+        <nav className="menu flex items-center gap-[30px]">
+            <ul className="flex items-center gap-[30px]">
+                <li><a href="#" className="linkMenu text-white text-sm uppercase">Home</a></li>
+                <li><a href="#" className="linkMenu text-white text-sm uppercase">Sobre mim</a></li>
+                <li><a href="#" className="linkMenu text-white text-sm uppercase">Tecnologias</a></li>
+                <li><a href="#" className="linkMenu text-white text-sm uppercase">Projetos</a></li>
+                <li><a href="#" className="linkMenu text-white text-sm uppercase">Skills</a></li>
             </ul>
 
-            <a href="" className="button"></a>
+            <a href="#" className="button">Entrar em contato</a>
         </nav>
     )
 }
